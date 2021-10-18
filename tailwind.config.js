d78b30e926a8f933e0fae3d8f69cc612c2791e9f
Tejeshwer Singh Sachdeva <tejeshwersingh2504@gmail.com>
2021-10-18 15:27:22 +0530

    Initial Commit
    - Created React App
    - Initialized the project with TailwindCSS
    - Added static book data

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      blue: '#1A659E',
+      black: '#191516',
+      ghostwhite: "#F7F7FF",
+      purewhite: '#FFFFFF'
+    },
+
+    fontFamily: {
+      heading: "'Scheherazade New', serif",
+      text: "'Nunito', sans-serif",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
